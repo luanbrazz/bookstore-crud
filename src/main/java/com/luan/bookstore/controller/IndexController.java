e96d22ac5912feb3803234e404ef1d86d26ebf45
Luan Braz <luan_silvacpv@hotmail.com>
2022-08-15 12:13:41 -0300

    first commit

---
+++ src/main/java/com/luan/bookstore/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.luan.bookstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/lista")
+    public ModelAndView getList(){
+        ModelAndView mv = new ModelAndView("index");
+        return mv;
+    }
+}
